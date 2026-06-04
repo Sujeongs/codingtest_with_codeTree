e6675b807e1c6b9f3ea2ae453a51f9287a18d97c
SuJeong <147980146+Sujeongs@users.noreply.github.com>
2026-06-04 23:16:23 +0900

    [Passed] 홀수 짝수에 따른 출력값 (52ms, 16MB)

--- trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -4,9 +4,8 @@ N = int(input())
 def tw(a):
     if a==1 or a==2:
         return a
-    if a%2==0:
-        return a+tw(a-2)
-    else:
-        return a+tw(a-2)
+
+    return a+tw(a-2)
+    
 
 print(tw(N))
